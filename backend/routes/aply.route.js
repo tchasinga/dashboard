d17ad2b31e436ad7a197d5a33512f5dd1164c9c6
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-15 10:39:28 +0200

    new changes

--- backend/routes/aply.route.js
+++ backend/routes/aply.route.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { createAply, getAply, getAplyById } from "../controllers/aply.controller.js";
 
+const router = express.Router(); // Move this line up to initialize the router first
+
 router.post("/create", createAply);
 router.get("/get", getAply);
 router.get("/get/:id", getAplyById);
 
-
-const router = express.Router();
 export default router;
