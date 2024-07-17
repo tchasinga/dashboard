734fb896ae038415f0ed32aa738fc4c7c2ac5e9a
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-17 14:21:05 +0200

    new code is added now

--- frontend/src/Components/Dashboard.jsx
+++ frontend/src/Components/Dashboard.jsx
@@ -1,6 +1,8 @@
 
 export default function Dashboard() {
   return (
-    <div className="mx-auto max-w-screen-xl">Dashboard</div>
+    <div className="mx-auto max-w-screen-xl">
+       <h1>Dashbord side </h1>
+    </div>
   )
 }
