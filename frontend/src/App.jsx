c598e7fdcc790a21e57175820940a56cc4c2a317
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-17 12:03:11 +0200

    new code is added own

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Dashboard from './Components/Dashboard'
 import SignIn from './Components/SignIn'
 import PrivateRoom from './Pages/PrivateRoom'
@@ -10,16 +10,17 @@ function App() {
   const currentUser = useSelector((state) => state.user && state.user.user.currentUser)
   return (
     <>
-   <BrowserRouter>
-    <Routes>
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/" element={<Home />} />
-      {currentUser ? <Route path="/dashboard" element={<Dashboard />} /> : <Navigate to="/signin" />}
-
-      <Route element={<PrivateRoom/>}>
-      </Route>
-    </Routes> 
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/" element={<Home />} />
+          <Route 
+            path="/dashboard" 
+            element={currentUser ? <Dashboard /> : <Navigate to="/signin" />}
+          />
+          <Route element={<PrivateRoom />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
