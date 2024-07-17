ea5482abfa92a23e18bfd91497e0c9ceb704957d
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-17 12:21:14 +0200

    new code is added now

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Dashboard from './Components/Dashboard'
 import SignIn from './Components/SignIn'
 import PrivateRoom from './Pages/PrivateRoom'
-import Home from './Components/Home'
+// import Home from './Components/Home'
 import { useSelector } from 'react-redux'
 import Header from './Components/Header'
 
@@ -15,7 +15,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/signin" element={<SignIn />} />
-          <Route path="/" element={<Home />} />
+          {/* <Route path="/" element={<Home />} /> */}
           <Route 
             path="/dashboard" 
             element={currentUser ? <Dashboard /> : <Navigate to="/signin" />}
--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,8 +1,14 @@
 
 export default function Header() {
   return (
-    <div>
-        <h1>Header</h1>
+    <div className="py-5">
+        <div className="mx-auto max-w-4xl">
+             
+             <div className=""></div>
+
+             <div className=""></div>
+
+        </div>
     </div>
   )
 }
