c4516651d18b60a576a9b9c809046e10f7532f9d
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-17 10:42:10 +0200

    new code is added now

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
       <Route path="/signin" element={<SignIn />} />
     
       <Route element={<PrivateRoom/>}>
-        <Route path="/dashborad" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Route>
     </Routes> 
     </BrowserRouter>
--- frontend/src/Components/SignIn.jsx
+++ frontend/src/Components/SignIn.jsx
@@ -1,5 +1,47 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { signInStart, singInSuccess, signInFailure } from '../redux/user/userSlice.js'
 
 export default function SignIn() {
+  const [formData, setFormData] = useState({})
+  const { loading } = useSelector(state => state.user && state.user.user)
+  const [showError, setShowError] = useState(false); // New state for error popup
+  const [showSuccess, setShowSuccess] = useState(false); // New state for success popup
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value })
+  }
+
+  const handlerSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      dispatch(signInStart())
+
+      const res = await fetch('https://blogs-sharing-ideas-api.onrender.com/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      })
+      const data = await res.json()
+      if (data.success === false) {
+        dispatch(signInFailure(setShowError(true)))
+        return
+      }
+      dispatch(singInSuccess(data))
+      setShowSuccess(true);
+      setShowError(false); 
+      navigate('/dashboard')
+
+    } catch (error) {
+      setShowError(true); // Show the Error component
+      dispatch(signInFailure(setShowError(true)))
+    }
+  }
   return (
     <div>SignIn</div>
   )
