657dd49f68b70581c5e682f360ed9fd497c1952f
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-17 14:42:27 +0200

    new chages

--- frontend/src/Components/Dashboard.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function Dashboard() {
-  return (
-    <div className="mx-auto max-w-screen-xl">
-       <h1>Dashbord side </h1>
-    </div>
-  )
-}
--- frontend/src/Components/Home.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-  return (
-    <div className="mx-auto max-w-screen-xl">
-        <h1>this is home</h1>
-    </div>
-  )
-}
