67744d86406e744181ea9acb1f0a23a48568eb55
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-17 14:17:36 +0200

    new side is added now

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,6 +1,8 @@
 
 export default function Home() {
   return (
-    <div className="mx-auto max-w-screen-xl"></div>
+    <div className="mx-auto max-w-screen-xl">
+        <h1>this is home </h1>
+    </div>
   )
 }
