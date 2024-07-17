48891589b512900cd095cb6f4a4a19c789f4ec30
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-17 12:37:21 +0200

    new code is added now

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,12 +1,31 @@
 
+import { FaHome } from "react-icons/fa";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+
+
 export default function Header() {
+    const currentUser = useSelector((state) => state.user && state.user.user.currentUser)
   return (
     <div className="py-5">
-        <div className="mx-auto max-w-4xl">
+        <div className="mx-auto max-w-6xl flex justify-between items-center w-full flex-wrap">
              
-             <div className=""></div>
+             <div className="text-xl">
+                <FaHome />
+             </div>
 
-             <div className=""></div>
+             <div className="">
+                {currentUser ? (
+                   <div className="flex justify-center items-center gap-4">
+                    <div className="text-lg underline">{currentUser.user.email}</div>
+                    <Link to="/dashboard" className="cursor-pointer ">
+                      <div className="text-white bg-green-800 p-2 flex justify-center rounded-[20px] w-[150px] hover:bg-orange-800 duration-1000">Admin page</div>
+                    </Link>
+                   </div>
+                ) : (
+                    <div className="text-lg">Welcome, Guest</div>
+                )}
+             </div>
 
         </div>
     </div>
