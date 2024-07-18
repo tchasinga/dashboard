4bf9bdc74e1cc1ecd6bfd6854ec69547bc75fbd6
Tchasinga jacques <tchasingajacques@gmail.com>
2024-07-18 18:25:56 +0200

    New commit is online on God

--- backend/db/mongoconnection.js
+++ backend/db/mongoconnection.js
@@ -1,19 +1,21 @@
-import dotenv from 'dotenv'
-import mongoose from 'mongoose'
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
 const urlmongoDB = process.env.MONGODB_URL;
-const mongoconnection =async () => {
+
+const mongoconnection = async () => {
   try {
-    mongoose
-    .connect(urlmongoDB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-      console.log("✅ Connected successfully to MongoDB");
-    })
+    await mongoose.connect(urlmongoDB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 25000, // Optional: Add a timeout for server selection
+    });
+    console.log("✅ Connected successfully to MongoDB");
   } catch (error) {
-    console.error("❌ Failed to connect to MongoDB:");
+    console.error("❌ Failed to connect to MongoDB:", error);
   }
-}
+};
 
-
-export default mongoconnection
+export default mongoconnection;
